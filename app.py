c5f65b49a3945e1816b879eace0b46019ff0041c
vallejojc3 <vallejojuanpablo27mail.com>
2026-01-07 17:31:33 -0500

    final

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta, date
-import os
+import os 
 import json
 from sqlalchemy.exc import OperationalError
 from dotenv import load_dotenv
